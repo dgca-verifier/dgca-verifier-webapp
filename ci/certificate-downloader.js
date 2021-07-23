5fdd774610912c9ae3e3de601bce0140bb28f6a0
Kael D'Alcamo <dalcamokael@gmail.com>
2021-07-23 22:23:03 +0200

    Wrote equivalent js of the python one

--- ci/certificate-downloader.js
+++ ci/certificate-downloader.js
@@ -5,16 +5,15 @@ const BASE_URL = 'https://get.dgc.gov.it/v1/dgc/signercertificate'
 const STATUS_ENDPOINT = 'status'
 const UPDATE_ENDPOINT = 'update'
 
-const headers = {
-  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-  'x-resume-token': '0'
-}
+const fetchCertificates = async (jsonTargetPath = './public/certificate-list.json') => {
+  const headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'x-resume-token': '0'
+  }
 
-const fetchCertificates = async () => {
   console.info('Fetching kid list...')
   const kidList = await fetch(`${BASE_URL}/${STATUS_ENDPOINT}`, { headers })
     .then(response => response.json())
-  console.debug(kidList)
 
   const certificateList = {}
 
@@ -29,14 +28,12 @@ const fetchCertificates = async () => {
 
     certificateList[kid] = await response.text()
     headers['x-resume-token'] = response.headers.get('x-resume-token')
-    console.info(`Current token: ${headers['x-resume-token']}`)
   }
 
   const certificateListJson = JSON.stringify(certificateList)
 
-  console.info('Storing certs on ./certificateList.json')
-  console.debug(certificateList)
-  await fs.writeFile('certificateList.json', certificateListJson)
+  console.info(`Storing certs on ${jsonTargetPath}`)
+  await fs.writeFile(jsonTargetPath, certificateListJson)
   console.info('Certs stored successfully')
 }
 
